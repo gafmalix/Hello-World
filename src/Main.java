1aca81936b40a8961c843f6c8df0cdb99ebec77d
gafmalix <79617087+gafmalix@users.noreply.github.com>
2023-04-04 20:20:24 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    
+    if (20 > 18) {
+    	  System.out.println("20 is greater than 18");
+    	}
+  }
+  
+}
